145141a23d209a2536680416f9acbb37c6ae759e
Laurent Peuch <cortex@worlddomination.be>
2018-07-15 06:23:45 +0200

    [mod] remove log output on cli

--- run.py
+++ run.py
@@ -147,8 +147,6 @@ async def run_job(worker, job):
             "data": model_to_dict(job),
         }, ["jobs", f"job-{job.id}"])
 
-        print(f">> {line}")
-
     # XXX stupid crap to stimulate long jobs
     await asyncio.sleep(random.randint(1, 15))
     # await asyncio.sleep(5)
