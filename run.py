268a3f8289b4fb760df341eae0b29575a8e4ded1
Alexandre Aubin <alex.aubin@mailoo.org>
2021-01-18 15:38:25 +0100

    Also allow collaborators to trigger jobs, because for some reason Github flag some member as collaborators...

--- run.py
+++ run.py
@@ -1032,7 +1032,7 @@ async def github(request):
 
     # We only accept this from people which are member/owner of the org/repo
     # https://docs.github.com/en/free-pro-team@latest/graphql/reference/enums#commentauthorassociation
-    if hook_infos["comment"]["author_association"] not in ["MEMBER", "OWNER"]:
+    if hook_infos["comment"]["author_association"] not in ["MEMBER", "OWNER", "COLLABORATOR"]:
         # Unauthorized
         abort(403, "Unauthorized")
 
