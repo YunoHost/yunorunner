ab24095d178a4606ef4719dd7913e46a4100f024
Félix Piédallu <felix@piedallu.me>
2025-04-17 11:28:52 +0200

    Master branch will be now main

--- maintenance/self_upgrade.sh
+++ maintenance/self_upgrade.sh
@@ -10,15 +10,17 @@ if [ "${0:0:1}" == "/" ]; then script_dir="$(dirname "$0")"; else script_dir="$(
 
 cd $script_dir/..
 
-# We only self-upgrade if we're in a git repo on master branch
+# We only self-upgrade if we're in a git repo on main branch
 # (which should correspond to production contexts)
 [[ -d ".git" ]] || exit
 
-[[ $(git rev-parse --abbrev-ref HEAD) == "master" ]] || exit
+[[ $(git rev-parse --abbrev-ref HEAD) == "master" ]] \
+|| [[ $(git rev-parse --abbrev-ref HEAD) == "main" ]] \
+|| exit
 
 git fetch origin --quiet
 
 # If already up to date, don't do anything else
-[[ $(git rev-parse HEAD) == $(git rev-parse origin/master) ]] && exit
+[[ $(git rev-parse HEAD) == $(git rev-parse origin/main) ]] && exit
 
-git reset --hard origin/master --quiet
+git reset --hard origin/main --quiet
